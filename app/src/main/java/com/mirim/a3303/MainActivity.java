8c317b35540f5283170601bc6709f4549fa783f3
ala0327 <ala0327@example.com>
2021-03-26 15:24:19 +0900

    Initial commit

---
+++ app/src/main/java/com/mirim/a3303/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mirim.a3303;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
