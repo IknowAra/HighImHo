91c10a7d916895de4e82cf953bc06b06ffa19b17
KimAra <s2019w06@e-mirim.hs.kr>
2021-04-20 04:03:02 +0900

    main 액티비티 이동, 다이얼로그 editText  구현 input_data xml

---
+++ app/src/main/java/com/mirim/a3303/InputData.java
@@ -0,0 +1,17 @@
+package com.mirim.a3303;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_data);
+    }
+    private String makeCipher(String key, String contents){
+
+    }
+}
